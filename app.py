912ab3a209b64319c80b532189aa35f6b8bdef2a
unknown <morgannlamort@gmail.com>
2025-05-16 10:20:30 +0200

    Redirection vers muscutracker après callback

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 def strava_redirect():
     code = request.args.get('code')
     if code:
-        # Redirige vers l'application Flutter (schéma personnalisé)
-        return redirect(f"muscutracker://auth/callback?code={code}")
-    return "❌ Aucun code reçu", 400
+        return redirect(f'muscutracker://auth/callback?code={code}')
+    return "Code manquant dans la requête", 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
